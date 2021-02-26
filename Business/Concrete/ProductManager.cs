29aa2c07d3312d9696f7982e5d7bac2eb0ebf7bb
YUSUF ERDEM <yusuf.erdem@bahcesehir.edu.tr>
2021-02-26 04:31:18 +0300

    9. ders sonu

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,41 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            return _productDal.GetProductDetails();
+        }
+
+        List<Product> IProductService.GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p=> p.CategoryId == id);
+        }
+
+        List<Product> IProductService.GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=> p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,42 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductTest();
+            //CategoryManager categoryManager = CategoryTest();
+            //CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            //categoryManager.GetById(2);
+
+
+
+        }
+
+        private static CategoryManager CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+            return categoryManager;
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName +"/" + product.CategoryName );
+            }
+        }
+    }
+}
