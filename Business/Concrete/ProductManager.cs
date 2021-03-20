e49246efda3cafa59731ab412f0f1f35d7fe1cc7
YUSUF ERDEM <yusuf.erdem@bahcesehir.edu.tr>
2021-03-20 22:01:36 +0300

    14. gün sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.CCS;
 using Business.Constans;
 using Business.ValidationRules.FluentValidation;
@@ -27,7 +28,7 @@ namespace Business.Concrete
             _productDal = productDal;
             _categoryService = categoryService;
         }      
-
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
@@ -44,7 +45,7 @@ namespace Business.Concrete
             
         }
 
-        public IDataResult<List<Product>> GetAll()
+        public IDataResult<List<Product>> GetAll() 
         {
             
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
--- Business/Constans/Messages.cs
+++ Business/Constans/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constans
@@ -14,5 +16,12 @@ namespace Business.Constans
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExists = "Bu isimde bir ürün zaten var";
         public static string CategoryLimitExceded = "kategori limiti aşıldı";
+        public static string AuthorizationDenied = "Giriş Reddedildi";
+        public static string UserRegistered="Kullanıcı kaydoldu";
+        public static string UserNotFound="Kullanıcı bulunamadı";
+        public static string PasswordError="şifre hatası";
+        public static string SuccessfulLogin="başarılı giriş";
+        public static string UserAlreadyExists="kullanıcı zaten var";
+        public static string AccessTokenCreated="erişim oluşturuldu";
     }
 }
