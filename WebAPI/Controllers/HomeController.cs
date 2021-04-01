28b75f583863066a607982ccd2f45ec5116ff13d
ysf3rdm <79582319+ysf3rdm@users.noreply.github.com>
2021-04-01 21:28:06 +0300

    commit son

---
+++ WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        ICategoryService _categoryDal;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryDal = categoryService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //Thread.Sleep(500);
+
+            var result = _categoryDal.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+
+            }
+
+            return BadRequest(result);
+
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _categoryDal.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
